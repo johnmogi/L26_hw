4745b03a6455868a64ba69eeae05886edb3bcbcc
johnmogi <anguru@gmail.com>
2019-11-11 21:05:51 +0200

    3

--- HW/base3/main.js
+++ HW/base3/main.js
@@ -1,35 +1,35 @@
 /// <reference path="jquery-3.4.1.js" />
 
 $(() => {
+  $("#bSync").click(() => {
+    var firstNum = parseInt(prompt("enter a number"));
+    var secondNum = parseInt(prompt("enter a number"));
 
+    // var randomstring = Math.floor(Math.random() * firstNum) + secondNum
 
-    $("#bSync").click(() => {
+    function randomIntFromInterval(min, max) {
+      // min and max included
+      return Math.floor(Math.random() * (firstNum - secondNum + 1) + secondNum);
+    }
 
-        var min = parseInt(prompt("enter a number"));
-        var max = parseInt(prompt("enter a number"));
+    $("#passMe").html(randomIntFromInterval);
+    // console.log(randomstring)
+  });
 
-        // var randomstring = Math.floor(Math.random() * firstNum) + secondNum
-
-        function randomIntFromInterval(min, max) { // min and max included 
-            return Math.floor(Math.random() * (max - min + 1) + min);
-        }
-
-        $("#passMe").html(randomIntFromInterval)
-        // console.log(randomstring)
+  $("#bAsync").click(() => {
+    makePassword(result => {
+      $("#passMe").html(result);
     });
+  });
 
-    //     $("#bAsync").click(() => {
-    //         makePassword(result => {
-    //             $("#passMe").html(result)
-    //         });
-
-    //     });
-
-    //     function makePassword(callback) {
-    //         setTimeout(() => {
-    //             var randomstring = Math.floor(Math.random() * 200)
-    //             callback(randomstring);
-    //         }, 2000);
-    //     }
-
+  function makePassword(callback1, callback2) {
+    var firstNum = parseInt(prompt("enter a number"));
+    var secondNum = parseInt(prompt("enter a number"));
+    setTimeout(() => {
+      let randomstring = Math.floor(
+        Math.random() * (firstNum - secondNum + 1) + secondNum
+      );
+      callback1(randomstring);
+    }, 2000);
+  }
 });
