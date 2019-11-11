e934e126498ad12c9b2e20ba6d8cffaf87372b55
johnmogi <anguru@gmail.com>
2019-11-11 19:51:01 +0200

    1@

--- HW/base1/main.js
+++ HW/base1/main.js
@@ -1,31 +1,27 @@
 /// <reference path="jquery-3.4.1.js" />
 
 $(() => {
+
+
     $("#bSync").click(() => {
         var randomstring = Math.random().toString(36).slice(-6);
+
         $("#passMe").html(randomstring)
         // console.log(randomstring)
     });
 
-
     $("#bAsync").click(() => {
-
-        getSomeAsyncData(result => {
-            var randomstring = Math.random().toString(36).slice(-6);
-            $("#passMe").html(randomstring)
-            // console.log("result: " + result); // קריאה אסינכרונית
+        makePassword(result => {
+            $("#passMe").html(result)
         });
 
     });
 
-    // פונקציה אסינכרונית
-    // פונקציה שהקוד שקורא לה לא ממתין לסיומה
-    function getSomeAsyncData(callback) {
-        setTimeout(() => { // אסינכרונית
-
-        }, 3000);
+    function makePassword(callback) {
+        setTimeout(() => {
+            var randomstring = Math.random().toString(36).slice(-6);
+            callback(randomstring);
+        }, 2000);
     }
-
-
 
 });
