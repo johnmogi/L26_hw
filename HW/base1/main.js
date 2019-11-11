4f5ed23adc58b5c46e8392c5f6139954dac6f627
johnmogi <anguru@gmail.com>
2019-11-11 15:58:31 +0200

    added base logic - not finished

--- HW/base1/main.js
+++ HW/base1/main.js
@@ -1,8 +1,31 @@
 /// <reference path="jquery-3.4.1.js" />
 
 $(() => {
-    $("button").click(() => {
+    $("#bSync").click(() => {
         var randomstring = Math.random().toString(36).slice(-6);
-        console.log(randomstring)
+        $("#passMe").html(randomstring)
+        // console.log(randomstring)
     });
+
+
+    $("#bAsync").click(() => {
+
+        getSomeAsyncData(result => {
+            var randomstring = Math.random().toString(36).slice(-6);
+            $("#passMe").html(randomstring)
+            // console.log("result: " + result); // קריאה אסינכרונית
+        });
+
+    });
+
+    // פונקציה אסינכרונית
+    // פונקציה שהקוד שקורא לה לא ממתין לסיומה
+    function getSomeAsyncData(callback) {
+        setTimeout(() => { // אסינכרונית
+
+        }, 3000);
+    }
+
+
+
 });
